46b74547f63474ea2b755a5d82a171e6c056546a
Ali Sbane <ali.sbane.dev@gmail.com>
2022-05-02 03:47:15 +0000

    Desktop version: Hero Section completed and states added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import heroMobileImg from "../assets/images/hero-img-mobile.png";
 import heroTabletImg from "../assets/images/hero-img-tablet.png";
+import heroDesktopImg from "../assets/images/hero-img-desktop.png";
 
 export default function Hero() {
   return (
@@ -8,6 +9,7 @@ export default function Hero() {
       <div className="hero__image">
         <img className="hero__img-m" src={heroMobileImg} alt="" />
         <img className="hero__img-t" src={heroTabletImg} alt="" />
+        <img className="hero__img-d" src={heroDesktopImg} alt="" />
       </div>
       <h1>
         Group Chat <br />
