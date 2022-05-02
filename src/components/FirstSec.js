89b9bd6a7afc60e962001ec49f4799da4888a049
Ali Sbane <ali.sbane.dev@gmail.com>
2022-05-02 16:37:06 +0000

    Mobile version: first section completed

--- src/components/FirstSec.js
+++ src/components/FirstSec.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+
+import firstImg from "../assets/images/fs-gird-1.png";
+import secondImg from "../assets/images/fs-gird-2.png";
+import thirdImg from "../assets/images/fs-gird-3.png";
+import fourthImg from "../assets/images/fs-gird-4.png";
 
 export default function FirstSec() {
   return (
-    <div></div>
-  )
+    <div className="first-sec">
+      <div className="section-number__css-icon">
+        <span className="section-number__css-icon__line"></span>
+        <div className="section-number__css-icon__circle">
+          <span className="section-number__css-icon__number">01</span>
+        </div>
+      </div>
+      <div className="first-sec__grid">
+        <img src={firstImg} alt="" />
+        <img src={secondImg} alt="" />
+        <img src={thirdImg} alt="" />
+        <img src={fourthImg} alt="" />
+      </div>
+      <h3 className="first-sec__h3">Built for modern use</h3>
+      <h2 className="first-sec__h2">Smarter meetings, all in one place</h2>
+      <p className="first-sec__p">
+        Send messages, share files, show your screen, and record your meetings —
+        all in one workspace. Control who can join with invite-only team access,
+        data encryption, and data export.
+      </p>
+    </div>
+  );
 }
--- src/components/SecondSec.js
+++ src/components/SecondSec.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export default function SecondSec() {
   return (
-    <div></div>
-  )
+    <div className="second-sec">
+      <div className="section-number__css-icon">
+        <span className="section-number__css-icon__line"></span>
+        <div className="section-number__css-icon__circle">
+          <span className="section-number__css-icon__number">02</span>
+        </div>
+      </div>
+      <h2 className="second-sec__h2">Experience more together</h2>
+      <p className="second-sec__p">
+        Stay connected with reliable HD meetings and unlimited one-on-one and
+        group video sessions.
+      </p>
+      <button className="second-sec__btn">
+        Download
+        <span className="second-sec__btn__version-number">v1.3</span>
+      </button>
+    </div>
+  );
 }
